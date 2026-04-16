103d27a90a778802cb0ae4840a418165ffdffd67
DanielW0021 <walaszekdaniel21@gmail.com>
2026-04-16 17:02:15 +0200

    improvement models.py

--- core/models.py
+++ core/models.py
@@ -1,25 +1,32 @@
-from dataclasses import dataclass, field
 import datetime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, MappedAsDataclass, relationship
+from sqlalchemy import ForeignKey
 
-@dataclass
-class Category:
-    name : str
-    icon : str
-    color : str
-    id : int | None = None
-@dataclass
-class Expense:
-    amount : float
-    description : str
-    category_id : int
-    date : datetime.date
-    id : int | None = None
-    created_at : datetime.datetime = field(default_factory=datetime.datetime.now)
+class Base(DeclarativeBase, MappedAsDataclass):
+    pass
+
+class Category(Base):
+    __tablename__ = "categories"
+    name: Mapped[str] = mapped_column()
+    icon: Mapped[str] = mapped_column()
+    color: Mapped[str] = mapped_column()
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+
+class Expense(Base):
+    __tablename__ = "expenses"
+    amount: Mapped[float] = mapped_column()
+    description: Mapped[str] = mapped_column()
+    category: Mapped[Category] = relationship(init=False)
+    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'))
+    date: Mapped[datetime.date] = mapped_column()
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    created_at: Mapped[datetime.datetime] = mapped_column(default_factory=datetime.datetime.now)
     
-@dataclass
-class Budget:
-    category_id : int
-    limit : float
-    month : int
-    year : int 
-    id : int | None = None
+
+class Budget(Base):
+    __tablename__ = "budgets"
+    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'))
+    limit: Mapped[float] = mapped_column()
+    month: Mapped[int] = mapped_column()
+    year: Mapped[int] = mapped_column()
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
