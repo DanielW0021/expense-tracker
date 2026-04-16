17b46b44f0d2edfa99b0a7386649e55a703879e2
DanielW0021 <walaszekdaniel21@gmail.com>
2026-04-16 17:04:39 +0200

    improvement database.py

--- core/database.py
+++ core/database.py
@@ -1,33 +1,9 @@
-from sqlalchemy import create_engine, Column, Integer, Float, String, Date, DateTime, ForeignKey
-from sqlalchemy.orm import declarative_base, sessionmaker
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from .models import Base
 
-Base = declarative_base()
+engine = sqlalchemy.create_engine('sqlite:///expenses.db')
+SessionLocal = sessionmaker(bind=engine)
 
-class CategoryORM(Base):
-    __tablename__ = 'categories'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    color = Column(String)
-    icon = Column(String)
-    
-class ExpenseORM(Base):
-    __tablename__ = 'expenses'
-    id = Column(Integer, primary_key=True)
-    amount = Column(Float)
-    description = Column(String)
-    category_id = Column(Integer, ForeignKey('categories.id'))
-    date = Column(Date)
-    created_at = Column(DateTime)
-    
-class BudgetORM(Base):
-    __tablename__ = 'budgets'
-    id = Column(Integer, primary_key=True)
-    category_id = Column(Integer)
-    limit = Column(Float)
-    month = Column(Integer)
-    year = Column(Integer)
-    
-engine = create_engine('sqlite:///expenses.db')
-Base.metadata.create_all(engine)
-
-SessionLocal = sessionmaker(bind=engine)            
+def init_db():
+    Base.metadata.create_all(bind=engine)
